ae0d84de9525618f5fd569fca66dbca6d7014a78
Mauro Styleman <mauro.styleman@users.noreply.github.com>
2024-12-25 23:45:31 +0100

    working on domain model

---
+++ backend/Domain/Day.cs
@@ -0,0 +1,9 @@
+﻿namespace Workplanner.Domain;
+
+public class Day
+{
+    Guid Id { get; set; }
+    DateOnly Date { get; set; }
+    List<Shift> Shifts { get; set; }
+    PlanningPeriod PlanningPeriod { get; set; }
+}
---
+++ backend/Domain/PlanningPeriod.cs
@@ -0,0 +1,8 @@
+﻿namespace Workplanner.Domain;
+
+public class PlanningPeriod
+{
+    Guid Id { get; set; }
+    DateTime Start { get; set; }
+    DateTime End { get; set; }
+}
